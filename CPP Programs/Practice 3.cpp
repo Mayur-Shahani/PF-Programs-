8571b323a1c6388767eee3304f8468d791c8a93f
shahanimkwork@gmail.com <shahanimkwork@gmail.com>
2025-01-21 04:49:45 +0500

    Patterns Practice

---
+++ CPP Programs/Practice 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//	int num = 1;
+	char ch = 'A';
+	for(int i = 1; i <= 3; i++)
+	{
+		
+		for(int j = 1; j <= 3; j++)\
+		{
+			cout << ch;
+			ch = ch + 1;
+		}
+		cout << endl;
+	}
+	
+}
