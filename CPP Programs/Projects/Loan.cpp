83d0a6cdfe8aeead74e2b809a907bc97483ce263
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-17 21:00:42 +0500

    Loan

---
+++ CPP Programs/Projects/Loan.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+   int age;
+   double income;
+   
+   cout << "ENTER YOUR AGE: ";
+   cin >> age;
+   
+   cout << "ENTER YOUR MONTHLY INCOME: ";
+   cin >> income;
+   
+   if( age >= 18)
+   { 
+     if( income >= 30000)
+     { 
+	   cout << " You are eligible for a loan." << endl;
+	   }
+	 else
+	 { 
+	   cout << " You are not eligible for a loan due to low income. " << endl;
+	 }
+   }
+   else
+   { 
+     cout << " You are not eligible for a loan due to age. "<< endl;
+   }
+   return 0;
+}
