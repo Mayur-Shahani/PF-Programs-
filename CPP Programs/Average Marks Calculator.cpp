c180bf7de564553bf660a0fe87bd286ab5c2cc1b
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-21 23:53:00 +0500

    Average Marks Calculator

---
+++ CPP Programs/Average Marks Calculator.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+struct member
+{
+	string name;
+	int age;
+	int marks[6];
+};
+int avg(int marks[]){
+	int sum = 0;
+	for (int i = 0; i < 6; i++) {
+		sum += marks[i];
+	}
+	
+	return sum;
+}
+int main() {
+	member members;
+	members.name = "AHMED";
+	members.age = 20;
+	for (int i = 0; i < 6;i++) {
+		cout << "Enter the marks of subject " << i + 1 << " : ";
+		cin >> members.marks[i];
+	}
+	int sum = avg(members.marks);
+	
+	cout << "The average marks of the student is : " << sum / 6 <<"%"<< endl;
+
+	return 0;
+}
