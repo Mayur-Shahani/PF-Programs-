74d626bc5a5f51dce0e4feedce17eb1e1ffe1244
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-23 22:42:29 +0500

    Even Number

---
+++ CPP Programs/Practice Questions/Even or Odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 5;
+	
+	if(x % 2 == 0)
+	{
+		cout << x << " is a even number ";
+	}
+	else
+	{
+		cout << x << " is a odd number";
+	}
+	return 0;
+}
