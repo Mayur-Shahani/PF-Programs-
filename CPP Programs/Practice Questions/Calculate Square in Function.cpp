ec7559021481d6785776d4eefce2894f8c219725
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-23 22:53:32 +0500

    Calculate Square Root

---
+++ CPP Programs/Practice Questions/Calculate Square in Function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ int square(int num)
+ {
+ 	return num * num;
+ }
+int main()
+{
+   int num;
+   cout << "Enter a number : ";
+   cin >> num;
+   
+   cout << square(num);	
+}
